0a104eb249ad368b3a8057be9885054e8c4ffe02
somrat <jafaruddin875@gmail.com>
2022-08-11 12:42:13 +0200

    working with custom input date

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { useState } from "react";
 import DatePicker from "react-datepicker"
+import { forwardRef } from "react";
 function App() {
   const [startDate, setStartDate] = useState(new Date());
-  
-    return(
-         <div>
-             <DatePicker selected={startDate} onChange={(date)=>{
-              setStartDate(date)
-              console.log(startDate)
-             }}/>
-         </div>
-
-    )
-}
+  const ExampleCustomInput = forwardRef(({ value, onClick }, ref) => (
+    <button className="example-custom-input" onClick={onClick} ref={ref}>
+      {value}
+    </button>
+  ));
+  return (
+    <DatePicker
+      selected={startDate}
+      onChange={(date) => setStartDate(date)}
+      customInput={<ExampleCustomInput />}
+    />
+  );
+};
 
 export default App
